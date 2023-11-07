a7f2c94e4b6b6c7250a71c0d9be942d6c725f8cc
dsapr <785722840@qq.com>
2023-11-07 22:54:41 +0800

    chore: article

--- components/home/content.js
+++ components/home/content.js
@@ -1,41 +1,40 @@
-import styled, { keyframes } from 'styled-components';
+import styled from "styled-components";
 
-import utilStyles from '../../styles/util.module.css';
-import styls from '../../styles/Content.module.css';
+import utilStyles from "../../styles/util.module.css";
+import styls from "../../styles/Content.module.css";
 
 const Box = styled.div`
-    width: 100px;
-    height: 1px;
-    background-color: #cccccc;
-    margin: 23px auto;
+  width: 100px;
+  height: 1px;
+  background-color: #cccccc;
+  margin: 23px auto;
 `;
 
-export default function Content () {
-
-
-  return ( 
+export default function Content() {
+  return (
     <>
       <div className={`${styls.content} ${utilStyles.plain}`}>
-            <article>
-              <p>
-                Hi, I&apos;m Dsapr, a sophomore at{' '}
-                <span className={utilStyles.stress}>
-                  Software Engineer&nbsp;
-                </span>
-                of Xi&apos;an.
-              </p>
-              <p>
-                I love <span className={utilStyles.stress}>cats</span>. 🐈
-              </p>
-            </article>
-          <div>
-        </div>
-        
-        <Box/>
-    
-        </div>
-        
-  </>
-    
+        <article>
+          <p>
+            Hi, I&apos;m Dsapr, a sophomore at{" "}
+            <span className={utilStyles.stress}>Software Engineer&nbsp;</span>
+            of Xi&apos;an. Most of my work is open source and publicly available
+            on{" "}
+            <a
+              className={`${styls.external}`}
+              target="_blank"
+              href="https://github.com/dsapr"
+            >
+              Github.
+            </a>
+          </p>
+          <p>
+            I love <span className={utilStyles.stress}>cats</span>. 🐈
+          </p>
+        </article>
+
+        <Box />
+      </div>
+    </>
   );
 }
